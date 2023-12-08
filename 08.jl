4f6b72bdeb024036f6a62401cb94f04a685a931b
Alexander Rauhut <alexraw@mailbox.org>
2023-12-08 16:27:49 +0100

    prettier 8

--- 08.jl
+++ 08.jl
@@ -1,26 +1,19 @@
 using Test
 using Base.Iterators: countfrom, cycle
 
-function walk(network::Dict, instr::Vector{Int}, init::AbstractString, isend::Function)::Int
+function walk(network::Dict, instr::String, init::AbstractString, isend::Function)::Int
     for (steps, i) in zip(countfrom(1), cycle(instr))
-        init = network[init][i]
+        init = network[init][i == 'L' ? 1 : 2]
         isend(init) && return steps
     end
 end
 
 function solve(path::String)::Tuple{Int, Int}
-    instr, network = split(read(path, String), "\n\n")
-    instr = findfirst.(collect(instr), "LR")
-    network = map(eachsplit(network, "\n", keepempty=false)) do line
-        node, c1, c2 = eachsplit(line, r"\W+", keepempty=false)
-        node => (c1, c2)
-    end |> Dict
-
-    part1 = walk(network, instr, "AAA", ==("ZZZ"))
-    part2 = map(collect(filter(endswith('A'), keys(network)))) do init
-        walk(network, instr, init, endswith('Z'))
-    end |> lcm
-    return part1, part2
+    dirs, _, nw... = readlines(path)
+    nw = Dict(node => (L, R) for (node, L, R) in eachsplit.(nw, r"\W+"))
+    part1 = walk(nw, dirs, "AAA", ==("ZZZ"))
+    part2 = [walk(nw, dirs, init, endswith('Z')) for init in filter(endswith('A'), keys(nw))]
+    return part1, lcm(part2)
 end
 
 @test solve("examples/08.txt") == (6, 6)
